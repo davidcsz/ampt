fcd02d86274bd64ba3a7dc0f5784156ac6e0aed0
dczanches <d.czanches@gmail.com>
2016-09-20 20:13:26 -0700

    Add response_type to appCredentials

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ let config = {
                 client_id: 'ef12d39900a84da787b2a9837a3e532b',
                 client_secret: '',
                 auth_url: 'https://accounts.spotify.com/authorize',
+                response_type: 'code',
                 scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public user-follow-read user-library-read user-read-private user-top-read',
                 redirect_uri: 'http://Davids-MBP:3000/spotify-auth'
             },
@@ -15,9 +16,9 @@ let config = {
                 client_id: '227ZX9',
                 client_secret: '',
                 auth_url: 'https://www.fitbit.com/oauth2/authorize',
+                response_type: 'code',
                 scope: 'activity heartrate location profile',
                 redirect_uri: 'http://Davids-MBP:3000/fitbit-auth'
-
             }
         } 
     }
