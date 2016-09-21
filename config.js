d662dcb6f98a9fcc068b1be30c3164c399b50790
dczanches <d.czanches@gmail.com>
2016-09-20 19:17:08 -0700

    Add dev config for app

---
+++ config.js
@@ -0,0 +1,19 @@
+let config = {
+    dev: {
+        server: {
+            port: 3000
+        },
+        appCredentials: {
+            spotify: {
+                client_id: '',
+                client_secret: ''
+            },
+            fitbit: {
+                client_id: '',
+                client_secret: ''
+            }
+        } 
+    }
+}
+
+module.exports = config;
