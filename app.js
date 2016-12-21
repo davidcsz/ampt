75117492c1980fdb0f9a74baeb3f29ab00cc5ec0
David Sanchez <d.czanches@gmail.com>
2016-12-20 21:53:49 -0800

    Fix fitbit-auth

--- app.js
+++ app.js
@@ -24,24 +24,28 @@ server.route({
     path: '/fitbit-auth',
     handler: function (request, reply) {
         if (request.query.code === undefined) {
-            console.log('Path hit:', request);
+            console.log(`Path hit: ${request.raw.url}`);
             console.log('Sending user to Fitbit auth page');
 
-            oauth.getAuthCode(config.dev.appCredentials.fitbit);
-            reply('Fitbit auth...');
+            let authUrl = oauth.getAuthCode(config.dev.appCredentials.fitbit);
+            reply.redirect(authUrl);
         } else if (request.query.code !== undefined) {
-            console.log('Path hit:', request);
+            console.log(`Path hit: ${request.raw.url}`);
             console.log('Got Fitbit auth code');
             console.log('Getting Fitbit access token');
 
-            oauth.getAccessToken(request.query.code, config.dev.appCredentials.Fitbit)
+            oauth.getAccessToken(request.query.code, config.dev.appCredentials.fitbit)
             .catch((accessTokenRequestError) => {
                 console.log('Failed to retrieve Fitbit access_token! Error: ' + accessTokenRequestError);
-                reply('Failed to get a Fitbit access token. :(')
+                reply('Failed to get a Fitbit access token. :(');
             })
             .then((accessTokenJson) =>{
-                console.log('Successfully retrieved Fitbit access_token: ' + accessTokenJson);
-                reply('Got Fitbit access token!');
+                if (accessTokenJson === undefined) {
+                    reply('Failed to get a Fitbit access token. :(');
+                } else if (accessTokenJson !== undefined) {
+                    console.log('Successfully retrieved Fitbit access_token: ' + accessTokenJson);
+                    reply('Got Fitbit access token!');
+                }
             });
         }
     }
@@ -56,8 +60,8 @@ server.route({
             console.log('Path hit:', request);
             console.log('Sending user to Spotify auth page');
 
-            oauth.getAuthCode(config.dev.appCredentials.spotify);
-            reply('Spotify auth...');
+            let authUrl = oauth.getAuthCode(config.dev.appCredentials.spotify);
+            reply.redirect(authUrl);
         } else if (request.query.code !== undefined) {
             console.log('Path hit:', request);
             console.log('Got Spotify auth code');
