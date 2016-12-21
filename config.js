afb0dc9f26f1b9f25a89305abe576589fee9f32e
David Sanchez <d.czanches@gmail.com>
2016-12-20 21:53:24 -0800

    Correct redirect_uri parameters

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ let config = {
                 auth_url: 'https://accounts.spotify.com/authorize',
                 response_type: 'code',
                 scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public user-follow-read user-library-read user-read-private user-top-read',
-                redirect_uri: 'http://Davids-MBP:3000/spotify-auth',
+                redirect_uri: 'http://davpc:3000/spotify-auth',
                 token_endpoint: 'https://accounts.spotify.com/api/token'
             },
             fitbit: {
@@ -19,7 +19,7 @@ let config = {
                 auth_url: 'https://www.fitbit.com/oauth2/authorize',
                 response_type: 'code',
                 scope: 'activity heartrate location profile',
-                redirect_uri: 'http://Davids-MBP:3000/fitbit-auth',
+                redirect_uri: 'http://davpc:3000/fitbit-auth',
                 token_endpoint: 'https://api.fitbit.com/oauth2/token'
             }
         } 
